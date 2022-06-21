9bb3107b04b4c8e91be9a6920c845188c53a4692
nazmieievilya <ilyanazmeev111@gmail.com>
2022-06-21 12:36:39 +0300

    conversion works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { CurrencyapidataService } from './currencyapidata.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'currency-converter';
+  currjson: any = [];
+
+  base = 'USD';
+  converted = 'UAH';
+  result: string = '1';
+  changeValues = '';
+  changebase(baseValue: string) {
+    this.base = baseValue;
+    console.log(this.base + ' ' + this.converted);
+  }
+
+  changeToBeConverted(convertedValue: string) {
+    this.converted = convertedValue;
+    console.log(this.base + ' ' + this.converted);
+  }
+  constructor(private currency: CurrencyapidataService) {}
+  convert() {
+    this.currency.getcurrencydata(this.base, this.converted).subscribe((data) => {
+      this.currjson = data
+      this.result = this.currjson.result   
+    });
+    
+
+  }
 }
---
+++ src/app/currencyapidata.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root',
+})
+export class CurrencyapidataService {
+  constructor(private http: HttpClient) {}
+  getcurrencydata(country1: string, country2: string) {
+    let url = 'https://api.exchangerate.host/convert?from=' + country1 + '&to=' + country2;
+    return this.http.get(url)
+  }
+}
