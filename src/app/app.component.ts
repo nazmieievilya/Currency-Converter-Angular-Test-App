5e3b85cc2d38cf7e04c9e07bf3b329612cf40aae
nazmieievilya <ilyanazmeev111@gmail.com>
2022-06-21 14:39:58 +0300

    autoReplacingCurrency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,27 +8,39 @@ import { CurrencyapidataService } from './currencyapidata.service';
 export class AppComponent {
   title = 'currency-converter';
   currjson: any = [];
-
   base = 'USD';
   converted = 'UAH';
   result: string = '1';
   changeValues = '';
+  firstAmount: number = 1;
+  secondAmount: number = 1;
   changebase(baseValue: string) {
+    if (this.converted == baseValue) {
+      var temporary = this.base;
+      this.base = this.converted;
+      this.converted = temporary;
+    }
     this.base = baseValue;
     console.log(this.base + ' ' + this.converted);
   }
 
   changeToBeConverted(convertedValue: string) {
+    if (this.base == convertedValue) {
+      var temporary = this.base;
+      this.base = this.converted;
+      this.converted = temporary;
+    }
     this.converted = convertedValue;
     console.log(this.base + ' ' + this.converted);
   }
   constructor(private currency: CurrencyapidataService) {}
   convert() {
-    this.currency.getcurrencydata(this.base, this.converted).subscribe((data) => {
-      this.currjson = data
-      this.result = this.currjson.result   
-    });
-    
-
+    this.currency
+      .getcurrencydata(this.base, this.converted)
+      .subscribe((data) => {
+        this.currjson = data;
+        var multiple = String(+this.firstAmount * +this.currjson.result);
+        this.result = multiple;
+      });
   }
 }
